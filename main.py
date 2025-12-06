d373c8b4fbf12479a65df57d9d4e411a54e8d346
SaltEmber <BL.UE086JAG@gmail.com>
2025-12-06 12:31:42 +1100

    Added letter counting feature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_letters
 
 def get_book_text(filepath):
-    with open(filepath) as f:
+    with open(filepath, encoding="utf-8-sig") as f:
         return f.read()
 
 def main():
@@ -10,4 +10,7 @@ def main():
 
     num_words = get_num_words(file_contents)
     print(f'Found {num_words} total words')
+
+    letters = get_num_letters(file_contents)
+    print(letters)
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,16 @@ def get_num_words(text):
     text = text.replace("\n", " ")
     words = text.split()
     return len(words)
+
+def get_num_letters(text):
+    text = text.replace("\n", "")
+
+    counting_dict = {}
+    for letter in text:
+        if not letter.lower() in counting_dict.keys():
+            counting_dict[letter.lower()] = 1
+            continue
+        counting_dict[letter.lower()] += 1
+    
+    return counting_dict
+        
