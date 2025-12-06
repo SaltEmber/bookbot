b495712e5e910203522bb5ee7159fa74d0f8cf91
SaltEmber <BL.UE086JAG@gmail.com>
2025-12-06 12:09:03 +1100

    Added count word functionality

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def count_words(text):
+    text = text.replace("\n", " ")
+    words = text.split()
+    return len(words)
+
+def main():
+    file_contents = get_book_text("books/frankenstein.txt")
+    print(file_contents)
+
+    num_words = count_words(file_contents)
+    print(f'Found {num_words} total words')
+main()
